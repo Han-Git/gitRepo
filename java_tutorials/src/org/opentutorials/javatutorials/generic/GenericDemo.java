4d1ea618a8a34bddb8d4d2d401ff5c6965a58aa7
Han <topofstar0@gmail.com>
2016-03-03 14:14:06 +0900

    java_tutorials
    #Generic
    #Restriction of Generic
    #Generic Extends

--- java_tutorials/src/org/opentutorials/javatutorials/generic/GenericDemo.java
+++ java_tutorials/src/org/opentutorials/javatutorials/generic/GenericDemo.java
@@ -1,31 +1,33 @@
 package org.opentutorials.javatutorials.generic;
 
-class EmployeeInfo{
+abstract class Info{
+	public abstract int getLevel();
+}
+
+class EmployeeInfo extends Info{
 	public int rank;
 	EmployeeInfo(int rank){
 		this.rank = rank;
 	}
+	public int getLevel() {
+		return this.rank;
+	}
 }
 
-class Person<T,S>{
+// restriction of Generic. In this case, you can only put which has Info as super class
+class Person<T extends Info>{
 	public T info;
-	public S id;
-	Person(T info, S id){
+	Person(T info){
 		this.info = info;
-		this.id = id;
 	}
-	
-	public <U> void printInfo(U info){
-        System.out.println(info);
-    }
 }
 
 public class GenericDemo {
 	public static void main(String[] args) {
-		EmployeeInfo e = new EmployeeInfo(1);
-		Integer id = new Integer(1);
-		// you can omit <Generic> 
-		Person p1 = new Person(e,id);	//Person<EmployeeInfo,Integer> p1 = new Person<EmployeeInfo,Integer>(e,id);
-		p1.printInfo(e);	// p1.<EmployeeInfo>printInfo(e);
+		Person p1 = new Person(new EmployeeInfo(12));
+		System.out.println(p1.info.getLevel());
+		
+		// error because it's not extended by Info
+		//Person<String> p2 = new Person<String>("∫Œ¿Â");
 	}
 }
