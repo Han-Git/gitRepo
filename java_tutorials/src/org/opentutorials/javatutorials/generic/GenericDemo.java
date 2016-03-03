d54bc848ee7335492d6b01b8fc6b474b43e67aaf
Han <topofstar0@gmail.com>
2016-03-03 10:52:44 +0900

    Java_tutorials
    #Generic demo

---
+++ java_tutorials/src/org/opentutorials/javatutorials/generic/GenericDemo.java
@@ -0,0 +1,30 @@
+package org.opentutorials.javatutorials.generic;
+
+class StudentInfo{
+	public int grade;
+	StudentInfo(int grade){
+		this.grade = grade;
+	}
+}
+
+class EmployeeInfo{
+	public int rank;
+	EmployeeInfo(int rank){
+		this.rank = rank;
+	}
+}
+
+class Person{
+	public Object info;
+	Person(Object info){
+		this.info = info;
+	}
+}
+
+public class GenericDemo {
+	public static void main(String[] args) {
+		Person p1 = new Person("∫Œ¿Â");
+		EmployeeInfo ei = (EmployeeInfo)p1.info;
+		System.out.println(ei.rank);
+	}
+}
