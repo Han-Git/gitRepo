720fcb1711463cfc2fad66e3e2a5bdede7f0d03e
Han <topofstar0@gmail.com>
2016-03-03 11:00:37 +0900

    java_tutorials
    #interfaces commit

---
+++ java_tutorials/src/org/opentutorials/javatutorials/interfaces/example1/A.java
@@ -0,0 +1,7 @@
+package org.opentutorials.javatutorials.interfaces.example1;
+
+public class A implements I{
+	public void z() {
+		
+	}
+}
---
+++ java_tutorials/src/org/opentutorials/javatutorials/interfaces/example1/I.java
@@ -0,0 +1,5 @@
+package org.opentutorials.javatutorials.interfaces.example1;
+
+public interface I {
+	public void z();
+}
